c2dccf61c7d0b9ccdfa43dbb715a38963a93b8b5
root <root@localhost.localdomain>
2019-07-26 15:50:54 +0300

    removing usernames from script

--- add_user_to_splunk.sh
+++ add_user_to_splunk.sh
@@ -2,6 +2,6 @@
 #first line is adding the user permanently to splunk group
 #second line is adding the user permanently to wheel group
 #in order for the script to work as expected you have to create a cronjob which is executed every minute
-/sbin/usermod -aG clientprojectteam aaam_vaishali.pohekar
-/sbin/usermod -aG wheel aaam_shalaka.sahay
+/sbin/usermod -aG clientprojectteam <username>
+/sbin/usermod -aG wheel <username>
 
